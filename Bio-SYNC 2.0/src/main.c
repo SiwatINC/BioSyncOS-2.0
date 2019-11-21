566aca3ed9869fd21a38c14236f038f1486e883a
Siwat Sirichai <siwat@siwatinc.com>
2019-11-21 11:12:19 +0700

    sts

---
+++ Bio-SYNC 2.0/src/main.c
@@ -0,0 +1,62 @@
+#include "esp_common.h"
+#include "freertos/task.h"
+#include "gpio.h"
+#include "uart.h"
+#include "i2c_master.h"
+#include "pwm.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <freertos/FreeRTOS.h>
+uint32 user_rf_cal_sector_set(void)
+{
+    flash_size_map size_map = system_get_flash_size_map();
+    uint32 rf_cal_sec = 0;
+    switch (size_map) {
+        case FLASH_SIZE_4M_MAP_256_256:
+            rf_cal_sec = 128 - 5;
+            break;
+
+        case FLASH_SIZE_8M_MAP_512_512:
+            rf_cal_sec = 256 - 5;
+            break;
+
+        case FLASH_SIZE_16M_MAP_512_512:
+        case FLASH_SIZE_16M_MAP_1024_1024:
+            rf_cal_sec = 512 - 5;
+            break;
+
+        case FLASH_SIZE_32M_MAP_512_512:
+        case FLASH_SIZE_32M_MAP_1024_1024:
+            rf_cal_sec = 1024 - 5;
+            break;
+
+        default:
+            rf_cal_sec = 0;
+            break;
+    }
+
+    return rf_cal_sec;
+}
+
+void loop(void* ignore)
+{
+    gpio16_output_conf();
+    while(true) {
+    	
+    }
+
+    vTaskDelete(NULL);
+}
+
+void user_init(void)
+{
+    uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity    = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
+    };
+    xTaskCreate(&loop, "loop", 2048, NULL, 1, NULL);
+}
